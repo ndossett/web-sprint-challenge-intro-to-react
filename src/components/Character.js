cd6408f6a7b1be2d439167239b756e560bbf2f32
ndossett <niki.m.dossett@gmail.com>
2020-11-06 13:11:49 -0500

    finished sprint

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,22 +4,25 @@ import styled from 'styled-components';
 
 const CharDiv = styled.div`
     border: 2px solid black;
-    width: 50%;
+    width: 40%;
     border-radius: 2px;
     text-align: center;
-    margin: 1rem;
+    margin: 1rem auto;
     color: #333;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
+    &:hover {
+        background-color: yellow;
+    }
 `
 
 const Character = ({characters}) => {
     return (
         <CharDiv>
             <h1>{characters.name}</h1>
-            <p>D.O.B: {characters.birth_year}      Gender: {characters.gender}</p>
+            <p>D.O.B: {characters.birth_year} <br></br> Gender: {characters.gender}</p>
         </CharDiv>
     )
 }
